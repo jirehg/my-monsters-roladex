a4b78e9dbdaf89e814c0c37adf53dd3ab38ffe22
Jireh <writer.jirehgibson@gmail.com>
2020-05-15 09:56:27 -0400

    Updates

---
+++ precache-manifest.020c29d09fa322248d6591669709627e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180b10bfce43f579f7b12e3dc226a939",
+    "url": "/my-monsters-roladex/index.html"
+  },
+  {
+    "revision": "5aac18c7a5defe189605",
+    "url": "/my-monsters-roladex/static/css/main.dfb0c77a.chunk.css"
+  },
+  {
+    "revision": "613daf5c86963bda9722",
+    "url": "/my-monsters-roladex/static/js/2.3f9764aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-monsters-roladex/static/js/2.3f9764aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aac18c7a5defe189605",
+    "url": "/my-monsters-roladex/static/js/main.9d11a46b.chunk.js"
+  },
+  {
+    "revision": "5143136b8ea8e56a94cf",
+    "url": "/my-monsters-roladex/static/js/runtime-main.b3af00ec.js"
+  }
+]);
